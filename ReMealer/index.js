414604ed5dfd9323e5f616009b0a01530e7a92be
Châu <tranhoaichau.00@gmail.com>
2021-02-19 22:12:07 +0700

    init app

---
+++ ReMealer/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+import Amplify from 'aws-amplify';
+import config from './src/aws-exports';
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+Amplify.configure(config);
+AppRegistry.registerComponent(appName, () => App);
